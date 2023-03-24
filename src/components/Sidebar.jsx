f13dccce381693678d5d4c55be88d0570ed7b0ae
William0832 <squall832@gmail.com>
2023-03-25 04:17:25 +0800

    🌈 style: sidebar logo 美化

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,26 @@
 import { Link } from "react-router-dom"
+import { Typography, List, ListItem } from '@mui/material'
 export default function Sidebar () {
+  const logoSize = 30
+
   return (
     <div id="sidebar" className="flex-col p-2 w-[200px] border-r-2">
-      <h1>FCOP</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to={'blueBook'}>BlueBook</Link>
-          </li>
-        </ul>
+      <div className="flex items-center justify-around">
+        <img
+          className={`w-[${logoSize}px] h-[${logoSize}]px`}
+          src="./logo.png" alt="FCOP logo"
+        />
+        <Typography variant="h6">FCOP</Typography>
+      </div>
+
+      <nav class>
+        <List>
+          <ListItem>
+            <Link to={'blueBook'}>
+              <Typography variant="h6">BlueBook</Typography>
+            </Link>
+          </ListItem>
+        </List>
       </nav>
     </div>
   )
